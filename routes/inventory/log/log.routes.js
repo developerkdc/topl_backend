29b3a149fe5b73531fda7b0ff372aae87e1062fd
AbhiKdc <abhishek@kdigitalcurry.com>
2024-09-19 19:22:38 +0530

    api created single invoice fetch and edit

--- routes/inventory/log/log.routes.js
+++ routes/inventory/log/log.routes.js
@@ -4,6 +4,7 @@ import {
   add_single_log_item_inventory,
   edit_log_invoice_inventory,
   edit_log_item_inventory,
+  edit_log_item_invoice_inventory,
   inward_sr_no_dropdown,
   item_sr_no_dropdown,
   listing_log_inventory,
@@ -16,6 +17,7 @@ const router = Router();
 router.post("/list-inventory", listing_log_inventory);
 router.post("/add-inventory", CheckRoleAndTokenAccess, add_log_inventory);
 router.post("/add-item-inventory", add_single_log_item_inventory);
+router.patch("/edit-invoice-item-inventory/:invoice_id", edit_log_item_invoice_inventory);
 router.patch("/edit-item-inventory/:item_id", edit_log_item_inventory);
 router.patch("/edit-invoice-inventory/:invoice_id", edit_log_invoice_inventory);
 router.post("/download-excel-log-logs", logLogsCsv);
