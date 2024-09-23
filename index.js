06888c6814dfaf467eadf2784969f645ffe14daa
AbhiKdc <abhishek@kdigitalcurry.com>
2024-09-23 19:25:36 +0530

    fleece inventory

--- index.js
+++ index.js
@@ -45,7 +45,6 @@ import plywoodInventoryRoutes from "./routes/inventory/plywood/plywood.routes.js
 import mdfInventoryRoutes from "./routes/inventory/mdf/mdf.routes.js";
 import faceInventoryRoutes from "./routes/inventory/face/face.routes.js";
 import coreInventoryRoutes from "./routes/inventory/core/core.routes.js";
-import fleecePaperRoutes from "./routes/inventory/fleecePaper/fleecePaper.routes.js";
 import crossCuttingFactoryRoutes from "./routes/factory/crossCutting/crossCutting.routes.js";
 import flitchingFactoryRoutes from "./routes/factory/flitching/flitching.routes.js";
 import logRouter from "./routes/inventory/log/log.routes.js"
@@ -55,6 +54,7 @@ import path from "path";
 import { globalErrorHandler } from "./utils/errors/GlobalErrorHandler.js";
 import flitch_router from "./routes/inventory/flitch/flitch.routes.js";
 import veneer_router from "./routes/inventory/veener/venner.routes.js";
+import fleece_router from "./routes/inventory/fleece/fleece.routes.js";
 const Configs = getConfigs();
 mongo_service();
 const app = express();
@@ -138,8 +138,8 @@ app.use(
 app.use(`/api/${Configs.server.version}/face-inventory`, faceInventoryRoutes);
 app.use(`/api/${Configs.server.version}/core-inventory`, coreInventoryRoutes);
 app.use(
-  `/api/${Configs.server.version}/fleecePaper-inventory`,
-  fleecePaperRoutes
+  `/api/${Configs.server.version}/fleece-inventory`,
+  fleece_router
 );
 
 //factory - routes
