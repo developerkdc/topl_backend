85b54bba96717cb506e27bee301cf91fa20b83a6
AbhiKdc <abhishek@kdigitalcurry.com>
2024-10-15 18:33:13 +0530

    expenses amount in iscc

--- routes/masters/Expenses/index.js
+++ routes/masters/Expenses/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import logExpenseRouter from "./logExpense.routes.js";
+import flitchExpenseRouter from "./flitchExpense.routes.js";
 const expenseRouter = express.Router();
 
 expenseRouter.use(logExpenseRouter)
+expenseRouter.use(flitchExpenseRouter)
 
 export default expenseRouter;
