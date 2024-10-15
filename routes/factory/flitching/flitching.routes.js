7d98e01150406ced5fdd8250aac8831ee2d31a67
govind717 <govindraj.penshanwar@kdigitalcurry.com>
2024-10-15 18:11:19 +0530

    15/10/2024 added flitching done export csv

--- routes/factory/flitching/flitching.routes.js
+++ routes/factory/flitching/flitching.routes.js
@@ -6,7 +6,9 @@ import {
   listing_issue_for_flitching,
   log_no_dropdown,
   revert_issue_for_flitching,
-  fetch_all_flitchings_by_issue_for_flitching_id
+  fetch_all_flitchings_by_issue_for_flitching_id,
+  revert_flitching_done_items,
+  flitchingDoneExcel
 } from "../../../controllers/factory/flitching/flitchingController.js";
 import CheckRoleAndTokenAccess from "../../../middlewares/permission.js";
 import AuthMiddleware from "../../../middlewares/verifyToken.js";
@@ -26,12 +28,16 @@ router.post(
 );
 
 router.post("/add-flitching", AuthMiddleware, RolesPermissions("flitching_factory", "create"), add_flitching_inventory);
-router.patch(
+router.post(
   "/edit-flitching/:id",
+  AuthMiddleware, RolesPermissions("flitching_factory", "edit"),
   edit_flitching_inventory
 );
 router.post("/list-flitching-done", AuthMiddleware, RolesPermissions("flitching_factory", "view"), listing_flitching_done_inventory);
 router.get("/log-no-dropdown", log_no_dropdown);
-router.get("/list-flitchings-by-issued-flitching-id/:id", AuthMiddleware, RolesPermissions("flitching_factory", "view"), fetch_all_flitchings_by_issue_for_flitching_id)
+router.get("/list-flitchings-by-issued-flitching-id/:id", AuthMiddleware, RolesPermissions("flitching_factory", "view"), fetch_all_flitchings_by_issue_for_flitching_id);
+router.post("/revert-flitching-done-items/:id", AuthMiddleware, revert_flitching_done_items);
+
+router.post("/download-flitching-done-excel", AuthMiddleware, RolesPermissions("flitching_factory", "view"), flitchingDoneExcel)
 
 export default router;
