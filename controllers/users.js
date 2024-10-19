f19ca8f555c59e29d7fcea5cbc1c19151d33ee4f
AkshayKDC <akshay.ingle@kdigitalcurry.com>
2024-10-19 13:30:48 +0530

    role api update

--- controllers/users.js
+++ controllers/users.js
@@ -263,7 +263,7 @@ export const AdminChangePassword = catchAsync(async (req, res) => {
 
 export const RoleNameList = catchAsync(async (req, res) => {
   const { dept_name } = req.body;
-  const role = await RolesModel.find({ dept_name: dept_name }, "_id role_name");
+  const role = await RolesModel.find({ dept_name: dept_name, status: true }, "_id role_name");
   if (!role) {
     return res.status(401).json({
       result: [],
