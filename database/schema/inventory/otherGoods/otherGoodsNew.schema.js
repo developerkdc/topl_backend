80a3b5d2a08275f5dad070aee1d83cdc07da4802
AkshayKDC <akshay.ingle@kdigitalcurry.com>
2024-10-19 18:24:22 +0530

    updated

--- database/schema/inventory/otherGoods/otherGoodsNew.schema.js
+++ database/schema/inventory/otherGoods/otherGoodsNew.schema.js
@@ -165,7 +165,7 @@ const othergoods_invoice_schema = new mongoose.Schema(
               name: {
                 type: String,
                 required: [true, "contact person name is required"],
-                unique: [true, "contact person name is required"],
+                // unique: [true, "contact person name is required"],
                 trim: true,
               },
               email: {
