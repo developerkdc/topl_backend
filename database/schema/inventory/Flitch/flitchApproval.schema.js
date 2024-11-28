4ae3c818766323ed0b0095bdd9b494a9d114bbfd
govind717 <govindraj.penshanwar@kdigitalcurry.com>
2024-11-28 12:50:27 +0530

    removed gst validation from flitch approval schema

--- database/schema/inventory/Flitch/flitchApproval.schema.js
+++ database/schema/inventory/Flitch/flitchApproval.schema.js
@@ -279,7 +279,8 @@ const flitch_approval_invoice_schema = new mongoose.Schema(
         },
         gst_number: {
           type: String,
-          required: [true, "gst number is required"],
+          // required: [true, "gst number is required"],
+          default: null
         },
         web_url: {
           type: String,
