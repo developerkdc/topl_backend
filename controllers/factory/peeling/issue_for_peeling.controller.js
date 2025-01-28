d5740605a5a09632b522e4528f2fd426f7b72e6a
AbhiKdc <abhishek@kdigitalcurry.com>
2025-01-28 17:27:32 +0530

    changes list issues for peeling

--- controllers/factory/peeling/issue_for_peeling.controller.js
+++ controllers/factory/peeling/issue_for_peeling.controller.js
@@ -8,12 +8,14 @@ import { StatusCodes } from '../../../utils/constants.js';
 import catchAsync from '../../../utils/errors/catchAsync.js';
 import { issues_for_status } from '../../../database/Utils/constants/constants.js';
 import mongoose from 'mongoose';
-import issue_for_peeling_model from '../../../database/schema/factory/peeling/issues_for_peeling.schema.js';
+import { issue_for_peeling_model } from '../../../database/schema/factory/peeling/issues_for_peeling.schema.js';
 import ApiResponse from '../../../utils/ApiResponse.js';
 import {
   crosscutting_done_model,
   crossCuttingsDone_view_modal,
 } from '../../../database/schema/factory/crossCutting/crosscutting.schema.js';
+import { DynamicSearch } from '../../../utils/dynamicSearch/dynamic.js';
+import { dynamic_filter } from '../../../utils/dymanicFilter.js';
 
 export const addIssueForPeelingFromLogInventory = catchAsync(
   async function (req, res, next) {
@@ -370,3 +372,161 @@ export const addIssueForPeelingFromCrosscutDone = catchAsync(
     }
   }
 );
+
+export const listing_issued_for_peeling = catchAsync(
+  async (req, res, next) => {
+    const {
+      page = 1,
+      limit = 10,
+      sortBy = 'updatedAt',
+      sort = 'desc',
+      search = '',
+    } = req.query;
+    const {
+      string,
+      boolean,
+      numbers,
+      arrayField = [],
+    } = req?.body?.searchFields || {};
+    const filter = req.body?.filter;
+
+    let search_query = {};
+    if (search != '' && req?.body?.searchFields) {
+      const search_data = DynamicSearch(
+        search,
+        boolean,
+        numbers,
+        string,
+        arrayField
+      );
+      if (search_data?.length == 0) {
+        return res.status(404).json({
+          statusCode: 404,
+          status: false,
+          data: {
+            data: [],
+          },
+          message: 'Results Not Found',
+        });
+      }
+      search_query = search_data;
+    }
+
+    const filterData = dynamic_filter(filter);
+
+    const match_query = {
+      ...filterData,
+      ...search_query,
+    };
+
+    // Aggregation stage
+    const aggCreatedByLookup = {
+      $lookup: {
+        from: 'users',
+        localField: 'created_by',
+        foreignField: '_id',
+        pipeline: [
+          {
+            $project: {
+              user_name: 1,
+              user_type: 1,
+              dept_name: 1,
+              first_name: 1,
+              last_name: 1,
+              email_id: 1,
+              mobile_no: 1,
+            },
+          },
+        ],
+        as: 'created_by',
+      },
+    };
+    const aggUpdatedByLookup = {
+      $lookup: {
+        from: 'users',
+        localField: 'updated_by',
+        foreignField: '_id',
+        pipeline: [
+          {
+            $project: {
+              user_name: 1,
+              user_type: 1,
+              dept_name: 1,
+              first_name: 1,
+              last_name: 1,
+              email_id: 1,
+              mobile_no: 1,
+            },
+          },
+        ],
+        as: 'updated_by',
+      },
+    };
+    const aggCreatedByUnwind = {
+      $unwind: {
+        path: '$created_by',
+        preserveNullAndEmptyArrays: true,
+      },
+    };
+    const aggUpdatedByUnwind = {
+      $unwind: {
+        path: '$updated_by',
+        preserveNullAndEmptyArrays: true,
+      },
+    };
+    const aggMatch = {
+      $match: {
+        ...match_query,
+      },
+    };
+    const aggSort = {
+      $sort: {
+        [sortBy]: sort === 'desc' ? -1 : 1,
+      },
+    };
+    const aggSkip = {
+      $skip: (parseInt(page) - 1) * parseInt(limit),
+    };
+    const aggLimit = {
+      $limit: parseInt(limit),
+    };
+
+    const listAggregate = [
+      aggCreatedByLookup,
+      aggCreatedByUnwind,
+      aggUpdatedByLookup,
+      aggUpdatedByUnwind,
+      aggMatch,
+      aggSort,
+      aggSkip,
+      aggLimit,
+    ]; // aggregation pipiline
+
+    const issue_for_peeling = await issue_for_peeling_model.aggregate(listAggregate);
+
+    const aggCount = {
+      $count: 'totalCount',
+    }; // count aggregation stage
+
+    const totalAggregate = [
+      aggCreatedByLookup,
+      aggCreatedByUnwind,
+      aggUpdatedByLookup,
+      aggUpdatedByUnwind,
+      aggMatch,
+      aggCount,
+    ]; // total aggregation pipiline
+
+    const totalDocument = await issue_for_peeling_model.aggregate(totalAggregate);
+
+    const totalPages = Math.ceil((totalDocument?.[0]?.totalCount || 0) / limit);
+
+    const response = new ApiResponse(200, 'Photo Data Fetched Successfully', {
+      data: issue_for_peeling,
+      totalPages: totalPages,
+    });
+    return res.status(200).json(response);
+  }
+);
+
+
--- routes/factory/peeling/issue_for_peeling.routes.js
+++ routes/factory/peeling/issue_for_peeling.routes.js
@@ -3,6 +3,7 @@ import AuthMiddleware from '../../../middlewares/verifyToken.js';
 import {
   addIssueForPeelingFromCrosscutDone,
   addIssueForPeelingFromLogInventory,
+  listing_issued_for_peeling,
 } from '../../../controllers/factory/peeling/issue_for_peeling.controller.js';
 const issueForPeelingRouter = express.Router();
 
@@ -15,6 +16,12 @@ issueForPeelingRouter.post(
   '/add-crosscut-done-issue-for-peeling',
   AuthMiddleware,
   addIssueForPeelingFromCrosscutDone
+);
+
+issueForPeelingRouter.post(
+  '/listing-issue-for-peeling',
+  AuthMiddleware,
+  listing_issued_for_peeling
 );
 
 export default issueForPeelingRouter;
