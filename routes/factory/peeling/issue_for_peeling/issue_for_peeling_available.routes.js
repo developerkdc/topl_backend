8d02691db50bb2b96f578768240deaeec62f11e5
AbhiKdc <abhishek@kdigitalcurry.com>
2025-02-03 18:51:46 +0530

    reflitching schema done

---
+++ routes/factory/peeling/issue_for_peeling/issue_for_peeling_available.routes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { fetch_issue_for_peeling_available_details } from '../../../../controllers/factory/peeling/issues_for_peeling/issue_for_peeling_available.controller.js';
+import AuthMiddleware from '../../../../middlewares/verifyToken.js';
+const issue_for_peeling_available = express.Router();
+
+issue_for_peeling_available.post(
+    '/list-issue-for-peeling-available',
+    AuthMiddleware,
+    fetch_issue_for_peeling_available_details
+  );
+
+export default issue_for_peeling_available;
