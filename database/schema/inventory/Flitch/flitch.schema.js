ff177d7b66eaf381331877afe9a04ebb1c318933
Abhishek Vishwakarma <141614535+AbhiKdc@users.noreply.github.com>
2025-02-25 11:26:22 +0530

    Update flitch.schema.js

--- database/schema/inventory/Flitch/flitch.schema.js
+++ database/schema/inventory/Flitch/flitch.schema.js
@@ -72,7 +72,7 @@ export const item_details_schema = new mongoose.Schema(
       trim: true,
       uppercase: true,
       default:null,
-      required:[true,'Log No Code is required'],
+      // required:[true,'Log No Code is required'],
     },
     flitch_formula: {
       type: String,
