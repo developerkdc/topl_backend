55e71fbf792ddb24d850ff8a038bef5fbb6009a4
govind717 <govindraj.penshanwar@kdigitalcurry.com>
2025-02-28 13:15:47 +0530

    added fetch all items by order id and added cancel item api

--- controllers/order/decorative_order.controller.js
+++ controllers/order/decorative_order.controller.js
@@ -72,6 +72,9 @@ export const update_decorative_order = catchAsync(async (req, res) => {
     const session = await mongoose.startSession()
     try {
         await session.startTransaction();
+        if (!order_details_id) {
+            throw new ApiError("Order details id is missing")
+        }
 
         for (let field of ["order_details", "item_details"]) {
             if (!req.body[field]) {
@@ -100,6 +103,7 @@ export const update_decorative_order = catchAsync(async (req, res) => {
         };
 
         const updated_item_details = item_details?.map((item) => {
+            item.order_id = order_details_result?._id;
             item.created_by = item.created_by ? item?.created_by : userDetails?._id;
             item.updated_by = userDetails?._id;
             item.createdAt = item.createdAt ? item?.createdAt : new Date();
@@ -321,11 +325,87 @@ export const fetch_all_decorative_order_items = catchAsync(
     }
 );
 
+
 export const fetch_all_decorative_order_items_by_order_id = catchAsync(async (req, res) => {
     const { id } = req.params;
 
+    if (!id) {
+        throw new ApiError("ID is missing", StatusCodes.BAD_REQUEST)
+    }
     if (!isValidObjectId(id)) {
         throw new ApiError("Invalid ID", StatusCodes.BAD_REQUEST)
     };
+
+    const pipeline = [
+        {
+            $match: {
+                _id: mongoose.Types.ObjectId.createFromHexString(id)
+            }
+        },
+        {
+            $lookup: {
+                from: 'decorative_order_item_details',
+                foreignField: "order_id",
+                localField: "_id",
+                as: "order_items_details"
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                foreignField: "_id",
+                localField: "created_by",
+                as: "created_user_details",
+                pipeline: [
+                    {
+                        $project: {
+                            first_name: 1,
+                            last_name: 1,
+                            user_name: 1,
+                            user_type: 1,
+                            email_id: 1,
+                        },
+                    }
+                ]
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                foreignField: "_id",
+                localField: "updated_by",
+                as: "updated_user_details",
+                pipeline: [
+                    {
+                        $project: {
+                            first_name: 1,
+                            last_name: 1,
+                            user_name: 1,
+                            user_type: 1,
+                            email_id: 1,
+                        },
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: {
+                path: "$created_user_details",
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $unwind: {
+                path: "$updated_user_details",
+                preserveNullAndEmptyArrays: true
+            }
+        },
+    ];
+
+    const result = await OrderModel.aggregate(pipeline);
+
+    const response = new ApiResponse(StatusCodes.OK, "All Items of order fetched successfully", result);
+    return res.status(StatusCodes.OK).json(response)
+
 
 })
--- routes/order/decorativeOrder.routes.js
+++ routes/order/decorativeOrder.routes.js
@@ -1,13 +1,14 @@
 import express from 'express';
-import { add_decorative_order, update_decorative_order, update_order_item_status_by_item_id, fetch_all_decorative_order_items } from '../../controllers/order/decorative_order.controller.js';
+import { add_decorative_order, update_decorative_order, update_order_item_status_by_item_id, fetch_all_decorative_order_items, fetch_all_decorative_order_items_by_order_id } from '../../controllers/order/decorative_order.controller.js';
 import AuthMiddleware from '../../middlewares/verifyToken.js';
 
 const decorativeOrderRouter = express.Router();
 
 // decorativeOrderRouter.get('/get-raw-order', AddRawOrder);
 decorativeOrderRouter.post('/add-order', AuthMiddleware, add_decorative_order);
-decorativeOrderRouter.post('/update-order', AuthMiddleware, update_decorative_order);
+decorativeOrderRouter.post('/update-order/:order_details_id', AuthMiddleware, update_decorative_order);
 decorativeOrderRouter.post('/list-decorative-orders', AuthMiddleware, fetch_all_decorative_order_items);
 decorativeOrderRouter.post('/cancel-order-item/:id', AuthMiddleware, update_order_item_status_by_item_id);
+decorativeOrderRouter.get('/fetch-decorative-orders-by-order-id/:id', AuthMiddleware, fetch_all_decorative_order_items_by_order_id);
 
 export default decorativeOrderRouter;
