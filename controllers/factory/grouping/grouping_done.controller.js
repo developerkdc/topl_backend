98d7836fb0eb13720323672388b9a06a96770a56
AbhiKdc <abhishek@kdigitalcurry.com>
2025-03-11 11:51:53 +0530

    changes and issues solved

--- controllers/factory/grouping/grouping_done.controller.js
+++ controllers/factory/grouping/grouping_done.controller.js
@@ -590,7 +590,7 @@ export const revert_all_grouping_done = catchAsync(async (req, res, next) => {
 export const group_no_dropdown = catchAsync(async (req, res, next) => {
   const { photo_no_id } = req.query;
   const matchQuery = {
-    is_damaged: true
+    is_damaged: false
   };
   if (photo_no_id) {
     matchQuery.photo_no_id = photo_no_id;
