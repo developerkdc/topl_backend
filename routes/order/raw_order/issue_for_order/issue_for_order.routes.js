1593e2944586f76021b407cf34683cc65d98e39f
govind717 <govindraj.penshanwar@kdigitalcurry.com>
2025-03-11 18:26:52 +0530

    added face order routes

--- routes/order/raw_order/issue_for_order/issue_for_order.routes.js
+++ routes/order/raw_order/issue_for_order/issue_for_order.routes.js
@@ -4,6 +4,7 @@ import flitch_inventory_order_router from './flitch_inventory/issue_flitch_inven
 import plywood_inventory_order_router from './plywood_inventory/issue_plywood_inventory_item_order.routes.js';
 import mdf_inventory_order_router from './mdf_inventory/issue_mdf_inventory_item_order.routes.js';
 import venner_inventory_order_router from './venner_inventory/issue_venner_inventory_item_order.routes.js';
+import face_inventory_order_router from './face_inventory/issue_face_inventory_item_order.routes.js';
 
 const issue_for_raw_order_router = Router();
 
@@ -27,6 +28,10 @@ issue_for_raw_order_router.use(
 issue_for_raw_order_router.use(
   '/issue-raw-venner-order',
   venner_inventory_order_router
+);
+issue_for_raw_order_router.use(
+  '/issue-raw-face-order',
+  face_inventory_order_router
 );
 
 // issue_for_raw_order_router.use('/issue-raw-plywood-order', plywood_inventory_order_router)
