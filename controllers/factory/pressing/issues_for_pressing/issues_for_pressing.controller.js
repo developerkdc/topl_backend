f6b9220ef4a6554c45f4bab719267b809fae6c0c
AbhiKdc <abhishek.vishwakarma@kdigitalcurry.com>
2025-04-17 21:21:12 +0530

    order listing doen

--- controllers/factory/pressing/issues_for_pressing/issues_for_pressing.controller.js
+++ controllers/factory/pressing/issues_for_pressing/issues_for_pressing.controller.js
@@ -820,6 +820,96 @@ export const fetch_all_issue_for_pressing_details = catchAsync(
         preserveNullAndEmptyArrays: true,
       },
     };
+    const aggOrderRelatedData = [
+      {
+        $lookup: {
+          from: "orders",
+          localField: "order_id",
+          pipeline: [
+            {
+              $project: {
+                order_no: 1,
+                owner_name: 1,
+                orderDate: 1,
+                order_category: 1,
+                series_product: 1
+              }
+            }
+          ],
+          foreignField: "_id",
+          as: "order_details"
+        }
+      },
+      {
+        $unwind: {
+          path: "$order_details",
+          preserveNullAndEmptyArrays: true
+        }
+      },
+      {
+        $lookup: {
+          from: "series_product_order_item_details",
+          localField: "order_item_id",
+          foreignField: "_id",
+          pipeline: [
+            {
+              $project: {
+                item_no: 1,
+                order_id: 1,
+                item_name: 1,
+                item_sub_category_name: 1,
+                group_no:1,
+                photo_number: 1
+              }
+            }
+          ],
+          as: "series_product_order_item_details"
+        }
+      },
+      {
+        $unwind: {
+          path: "$series_product_order_item_details",
+          preserveNullAndEmptyArrays: true
+        }
+      },
+      {
+        $lookup: {
+          from: "decorative_order_item_details",
+          localField: "order_item_id",
+          pipeline: [
+            {
+              $project: {
+                item_no: 1,
+                order_id: 1,
+                item_name: 1,
+                item_sub_category_name: 1,
+                group_no:1,
+                photo_number: 1
+              }
+            }
+          ],
+          foreignField: "_id",
+          as: "decorative_order_item_details"
+        }
+      },
+      {
+        $unwind: {
+          path: "$decorative_order_item_details",
+          preserveNullAndEmptyArrays: true
+        }
+      },
+      {
+        $addFields: {
+          order_item_details: {
+            $cond: {
+              if: { $ne: [{ $type: "$decorative_order_item_details" }, "missing"] },
+              then: "$decorative_order_item_details",
+              else: "$series_product_order_item_details"
+            }
+          }
+        }
+      }
+    ]
     const aggMatch = {
       $match: {
         ...match_query,
@@ -845,6 +935,7 @@ export const fetch_all_issue_for_pressing_details = catchAsync(
       aggCreatedByUnwind,
       aggUpdatedByLookup,
       aggUpdatedByUnwind,
+      ...aggOrderRelatedData,
       aggMatch,
       aggSort,
       aggSkip,
@@ -866,6 +957,7 @@ export const fetch_all_issue_for_pressing_details = catchAsync(
       aggCreatedByUnwind,
       aggUpdatedByLookup,
       aggUpdatedByUnwind,
+      ...aggOrderRelatedData,
       aggMatch,
       aggCount,
     ]; // total aggregation pipiline
