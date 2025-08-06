137ff52cf30c3a1d7536d9584b5d79bbd6219a3a
AbhiKdc <abhishek.vishwakarma@kdigitalcurry.com>
2025-08-06 17:01:27 +0530

    issue solved

--- controllers/order/raw_order/issue_for_order/grouping_factory/issue_for_order.controller.js
+++ controllers/order/raw_order/issue_for_order/grouping_factory/issue_for_order.controller.js
@@ -72,7 +72,7 @@ export const add_issue_for_order = catchAsync(async (req, res) => {
     if (
       Number(
         validate_sheets_for_order?.total_sqm +
-          Number(grouping_item_details?.issued_sqm)
+        Number(grouping_item_details?.issued_sqm)
       ) > order_item_data?.sqm
     ) {
       throw new ApiError(
@@ -187,13 +187,14 @@ export const add_issue_for_order = catchAsync(async (req, res) => {
         [
           {
             ...grouping_data,
-            order_id: issue_for_order_id,
+            order_id: order_item_data?.order_id,
             order_item_id: order_item_data?._id,
             issue_status: issues_for_status?.order,
             grouping_done_item_id: grouping_item_data?._id,
             grouping_done_other_details_id:
               grouping_item_data?.grouping_done_other_details_id,
             no_of_sheets: issued_sheets_for_order,
+            issue_status: issues_for_status?.order,
             sqm: issued_sqm_for_order,
             amount: issued_amount_for_order,
             created_by: userDetails?._id,
