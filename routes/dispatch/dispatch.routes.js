09138e9a4737bd4ea73dedc1e8e29698da3b8931
AkshayKDC <akshay.ingle@kdigitalcurry.com>
2025-11-07 11:29:55 +0530

    ewaybill update

--- routes/dispatch/dispatch.routes.js
+++ routes/dispatch/dispatch.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import AuthMiddleware from '../../middlewares/verifyToken.js';
-import { add_dispatch_details, cancel_dispatch_details, edit_dispatch_details, fetch_all_details_by_dispatch_id, fetch_all_dispatch_details, fetch_all_dispatch_items_details, fetch_dispatch_details_by_invoice_no, fetch_invoices, fetch_packing_details_by_customer_id, fetch_purchase_history, fetch_single_dispatch_items, generate_invoice_no, generate_irn_no, invoice_no_dropdown, load_packing_details, packing_done_dropdown, revert_dispatch_details } from '../../controllers/dispatch/dispatch.controller.js';
+import { add_dispatch_details, cancel_dispatch_details, cancel_irn_no, edit_dispatch_details, fetch_all_details_by_dispatch_id, fetch_all_dispatch_details, fetch_all_dispatch_items_details, fetch_dispatch_details_by_invoice_no, fetch_invoices, fetch_packing_details_by_customer_id, fetch_purchase_history, fetch_single_dispatch_items, generate_ewaybill_using_irn_no, generate_invoice_no, generate_irn_no, get_irn_by_doc, invoice_no_dropdown, load_packing_details, packing_done_dropdown, revert_dispatch_details } from '../../controllers/dispatch/dispatch.controller.js';
 import { dispatch_invoice_pdf } from '../../controllers/dispatch/dispatch_invoice.controller.js';
 import EInvoiceAuthMiddleware from '../../middlewares/eInvoiceAuth.middleware.js';
 const dispatchRouter = express.Router();
@@ -20,7 +20,9 @@ dispatchRouter.post("/generate-invoice-no", AuthMiddleware, generate_invoice_no)
 
 // eway bill and Irn apis
 dispatchRouter.post("/generate-irn-no/:id", AuthMiddleware, EInvoiceAuthMiddleware, generate_irn_no);
-// dispatchRouter.post("/generate-ewaybill-using-irn", AuthMiddleware, generate_ewaybill_using_irn_no);
+dispatchRouter.post("/get-irn-by-doc/:id", AuthMiddleware, EInvoiceAuthMiddleware, get_irn_by_doc);
+dispatchRouter.post("/cancel-irn-no/:id", AuthMiddleware, EInvoiceAuthMiddleware, cancel_irn_no);
+dispatchRouter.post("/generate-ewaybill-using-irn", AuthMiddleware, generate_ewaybill_using_irn_no);
 
 
 //mobile api
